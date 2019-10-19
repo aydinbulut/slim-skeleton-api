12e98d089a39b0cfdf86eaea337b2153ca0fd983
Aydın <abu@emakina.fr>
2019-10-19 16:26:25 +0300

    Install Symfony Dotenv package and integrate

--- src/settings.php
+++ src/settings.php
@@ -1,16 +1,20 @@
 <?php
 
 use Monolog\Logger;
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/.env');
 
 return [
     'displayErrorDetails' => true, // Should be set to false in production
     'db' => [
         'driver' => 'mysql',
-        'host' => 'slim-db',
-        'port' => '3306',
-        'database' => 'slimphp',
-        'username' => 'root',
-        'password' => 'root',
+        'host' => $_ENV['DB_HOST'],
+        'port' => $_ENV['DB_PORT'],
+        'database' => $_ENV['DB_NAME'],
+        'username' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix' => '',
